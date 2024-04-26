69adae7912305688020310ccc50769af408bec0c
ZahraNov1998 <z.mohammadi1998@gmail.com>
2024-04-26 21:31:21 +0330

    add logger

---
+++ Informer/Utilities/Utility.cs
@@ -0,0 +1,16 @@
+﻿namespace Informer.Utilities
+{
+    public class Utility
+    {
+        private readonly static string _filePath = @"log.txt";
+
+        public static string SetLog(string message)
+        {
+            File.AppendAllText(_filePath, $"\n{message}-{DateTime.Now}");
+
+            File.AppendAllText(_filePath, "\n-----------------------------------------------");
+
+            return "Internal Server Error";
+        }
+    }
+}
